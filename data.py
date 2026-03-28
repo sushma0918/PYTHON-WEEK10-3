f3afe674a96e3e87004de3858e581a3f2afc07d4
sushma0918 <sushmamanapuram18@gmail.com>
2026-03-28 20:51:39 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+#24331A05G8
 import pandas as pd
 df = pd.read_csv("C:\\Users\\91798\\PycharmProjects\\.venv\\Lib\\site-packages\\numpy\\_core\\tests\\data\\umath-validation-set-tanh.csv")
 df["new_column"] = df[df.columns[0]] * 2
